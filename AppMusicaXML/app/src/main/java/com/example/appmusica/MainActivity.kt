9713021e64c7c164d8d5ab8d6f745cda9590de12
Snaider <snaiderviracocha@gmail.com>
2024-09-30 22:01:44 +0200

    Finalizado
    Falta la parte de documentarla

--- AppMusicaXML/app/src/main/java/com/example/appmusica/MainActivity.kt
+++ AppMusicaXML/app/src/main/java/com/example/appmusica/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.appmusica
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.ImageButton
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var botonPlay: ImageButton
+    private lateinit var botonPause: ImageButton
+    private var reproduciendo: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        botonPlay = findViewById(R.id.imageButton)
+        botonPause = findViewById(R.id.imageButtonPause)
+
+        botonPlay.setOnClickListener {
+            reproduciendo = true
+            updateButtonVisibility()
         }
+
+        botonPause.setOnClickListener {
+            reproduciendo = false
+            updateButtonVisibility()
+        }
+    }
+
+    private fun updateButtonVisibility() {
+        if (!reproduciendo) {
+            botonPlay.visibility = View.VISIBLE
+            botonPause.visibility = View.INVISIBLE
+        } else {
+            botonPlay.visibility = View.INVISIBLE
+            botonPause.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("reproduciendo", reproduciendo)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        reproduciendo = savedInstanceState.getBoolean("reproduciendo", false)
+        updateButtonVisibility()
     }
 }
